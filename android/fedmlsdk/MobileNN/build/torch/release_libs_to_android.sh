5945cbd1b177840586af128f5ca82aa0151517c4
han-shanshan <sshan0731@hotmail.com>
2022-12-21 02:24:04 -0800

    delete unnecessary files

--- android/fedmlsdk/MobileNN/build/torch/release_libs_to_android.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp -rf ../../pytorch/build_android/lib/*.so ../../../libs/torch/arm64-v8a
--- python/fedml/core/dp/mechanisms/base_dp_mechanism.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseDPMechanism(ABC):
-    @abstractmethod
-    def compute_noise(self, size):
-        pass
